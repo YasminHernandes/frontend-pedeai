38e77a9bf6ae04c4b284f1763ee95a513a13d1db
Yasmin <yasminhernandesjobs@gmail.com>
2025-03-06 13:40:20 -0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { SignUpRestaurant } from './pages/index.ts'
+import App from './App.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>
+  },
+  {
+    path: '/public/restaurants',
+    element: <SignUpRestaurant/>
+  },
+])
+
+
+createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+)
